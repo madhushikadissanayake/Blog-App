181771ec0627e82d4ed0a93eaf3974aaaad56238
Madhushika Dissanayake <madhushikadissanayake59@gmail.com>
2025-07-04 15:36:36 +0530

    project uploded

---
+++ app/api/blog/route.js
@@ -0,0 +1,52 @@
+import { ConnectDB } from "@/lib/config/db"
+import BlogModel from "@/lib/models/BlogModel";
+const { NextResponse } = require("next/server")
+import {writeFile} from 'fs/promises'
+
+const LoadDB = async () => {
+    await ConnectDB();
+}
+
+LoadDB();
+
+// API Endpoint to get all blogs
+export async function GET(request) {
+
+    const blogId = request.nextUrl.searchParams.get("id");
+    if (blogId) {
+        const blog = await BlogModel.find({blogId});
+        return NextResponse.json(blog);
+    }
+    else{
+       const blogs = await BlogModel.find({});
+       return NextResponse.json({blogs}) 
+    }
+}
+
+// API Endpoint for uploading blogs
+export async function POST(request){
+
+    const formData = await request.formData();
+    const timestamp = Date.now();
+
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path,buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
+  
+    const blogData = {
+        title: `${formData.get('title')}`,
+        description: `${formData.get('description')}`,
+        category: `${formData.get('category')}`,
+        author: `${formData.get('author')}`,
+        image: `${imgUrl}`,
+        author_img: `${formData.get('author_img')}`    
+    }
+
+    await BlogModel.create(blogData);
+    console.log("Blog Saved");
+
+    return NextResponse.json({success:true,msg:"Blog Added"})
+}
